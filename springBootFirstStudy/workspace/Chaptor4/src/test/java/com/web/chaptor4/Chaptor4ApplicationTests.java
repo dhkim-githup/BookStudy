318385e28109064e29a9ed4a406d9bbdd452e347
DESKTOP-UEGKJGF\PC <good.dhkim@gmail.com>
2021-09-05 23:51:16 +0900

    File Up

---
+++ springBootFirstStudy/workspace/Chaptor4/src/test/java/com/web/chaptor4/Chaptor4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.chaptor4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chaptor4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
